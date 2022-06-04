7d446be78b6a89e768b39b519920831362162406
sahar hooshmand <94601788+saharhmz@users.noreply.github.com>
2022-06-04 16:04:07 +0430

    Update CustomerFile.java
    customer file edited

--- SnappFood/src/main/java/model/CustomerFile.java
+++ SnappFood/src/main/java/model/CustomerFile.java
@@ -1,19 +1,20 @@
 package model;
 
 import java.io.*;
+import java.util.ArrayList;
 
 public class CustomerFile {
 
     private File file;
 
     public int getFileSize(){
-        // tedad user hara barmigardanad
         file = new File("customer.txt");
         int size=0;
         try (BufferedReader reader = new BufferedReader(new FileReader(file))){
             reader.readLine();
             while (reader.readLine() != null){
                 size++;
+                reader.readLine();
                 reader.readLine();
                 reader.readLine();
                 reader.readLine();
@@ -38,6 +39,8 @@ public class CustomerFile {
             writer.write(customer.getPassword());
             writer.newLine();
             writer.write(customer.getPhoneNumber());
+            writer.newLine();
+            writer.write("0");
         }catch (IOException e){
             System.out.println(e.getMessage());
         }
@@ -56,6 +59,7 @@ public class CustomerFile {
                 if(emailAddress.equals(reader.readLine())){
                     return false;
                 }
+                reader.readLine();
                 reader.readLine();
                 reader.readLine();
             }
@@ -79,6 +83,7 @@ public class CustomerFile {
                 }
                 reader.readLine();
                 reader.readLine();
+                reader.readLine();
             }
 
 
@@ -100,9 +105,12 @@ public class CustomerFile {
                 if(username.equals(reader.readLine())){
                     password=reader.readLine();
                     phoneNumber=reader.readLine();
-                    return (new Customer(name,family,username,password,phoneNumber));
+                    Customer customer =new Customer(name,family,username,password,phoneNumber);
+                    customer.setWallet(reader.readLine());
+                    return (customer);
                 }
                 else {
+                    reader.readLine();
                     reader.readLine();
                     reader.readLine();
                 }
@@ -112,5 +120,92 @@ public class CustomerFile {
             System.out.println(e.getMessage());
         }
         return null;
+    }
+
+    public void increaseWallet(Customer customer ,String amount){
+        int size = getFileSize();
+        file = new File("customer.txt");
+        int wallet=0;
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))){
+            reader.readLine();
+            for(int i=0 ; i < size ; i++){
+                reader.readLine();
+                reader.readLine();
+                if(reader.readLine().equals(customer.getUsername())){
+                    reader.readLine();
+                    reader.readLine();
+                    wallet = Integer.parseInt(reader.readLine());
+                    wallet += Integer.parseInt(amount);
+                }
+                else {
+                    reader.readLine();
+                    reader.readLine();
+                    reader.readLine();
+                }
+            }
+
+
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+
+        writeInFileAfterEditing(customer,wallet);
+    }
+
+    public ArrayList<Customer> getAllCustomers(){
+        int size = getFileSize();
+        file = new File("customer.txt");
+        ArrayList<Customer> customers = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))){
+            reader.readLine();
+            for(int i=0 ; i < size ; i++){
+                Customer customer = new Customer(reader.readLine(),reader.readLine(),reader.readLine(),reader.readLine(),reader.readLine());
+                customer.setWallet(reader.readLine());
+                customers.add(customer);
+            }
+
+
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+        return customers;
+    }
+
+
+    public void writeInFileAfterEditing(Customer customer , int wallet){
+        ArrayList<Customer> customers = new ArrayList<>();
+        customers.addAll(getAllCustomers());
+        file= new File("customer.txt");
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))){
+
+            for(int i=0 ; i < customers.size() ; i++){
+                writer.newLine();
+                writer.write(customers.get(i).getName());
+                writer.newLine();
+                writer.write(customers.get(i).getFamily());
+                writer.newLine();
+                if(customers.get(i).getUsername().equals(customer.getUsername())){
+                    writer.write(customers.get(i).getUsername());
+                    writer.newLine();
+                    writer.write(customers.get(i).getPassword());
+                    writer.newLine();
+                    writer.write(customers.get(i).getPhoneNumber());
+                    writer.newLine();
+                    writer.write(String.valueOf(wallet));
+                }
+                else {
+                    writer.write(customers.get(i).getUsername());
+                    writer.newLine();
+                    writer.write(customers.get(i).getPassword());
+                    writer.newLine();
+                    writer.write(customers.get(i).getPhoneNumber());
+                    writer.newLine();
+                    writer.write(customers.get(i).getWallet());
+                }
+            }
+
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
     }
 }
