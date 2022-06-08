306ce6c4988b1ee37698f839c3f053cea9fedf77
Z.m_0_0_ <94681376+Zahramehdipoor@users.noreply.github.com>
2022-06-08 15:12:05 +0430

    Update Place.java

--- SnappFood/src/main/java/model/Place.java
+++ SnappFood/src/main/java/model/Place.java
@@ -1,4 +1,28 @@
 package model;
 
 public class Place {
+
+    private String name;
+    private String address;
+
+    public Place(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
