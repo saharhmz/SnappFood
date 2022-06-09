93c1ce1a7cb1597d461cde17b4fbceb19220d945
sahar hooshmand <94601788+saharhmz@users.noreply.github.com>
2022-06-09 18:03:45 +0430

    Update SendRequestFile.java

--- SnappFood/src/main/java/model/SendRequestFile.java
+++ SnappFood/src/main/java/model/SendRequestFile.java
@@ -4,6 +4,7 @@ import controller.RegisterFirstPageController;
 import controller.SendEmailController;
 
 import java.io.*;
+import java.util.ArrayList;
 
 public class SendRequestFile {
 
@@ -17,7 +18,6 @@ public class SendRequestFile {
             reader.readLine();
             while (reader.readLine() != null){
                 size++;
-                reader.readLine();
             }
         }catch (IOException e){
             System.out.println(e.getMessage());
@@ -26,7 +26,7 @@ public class SendRequestFile {
     }
 
     // customer-friend
-    public boolean requestIsExist(String friendUsername ){
+    public boolean requestIsExistForFriend(String friendUsername ){
         int size = getFileSize();
         file= new File("sendRequestToFriend.txt");
         try (BufferedReader reader = new BufferedReader(new FileReader(file))){
@@ -34,6 +34,24 @@ public class SendRequestFile {
             for(int i=0 ; i < size ; i++){
                 String[] friends =reader.readLine().split("-");
                 if( friendUsername.equals(friends[1])){
+                    return true;
+                }
+            }
+
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+        return false;
+    }
+
+    public boolean requestIsExistForCustomer(String customerUsername ){
+        int size = getFileSize();
+        file= new File("sendRequestToFriend.txt");
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))){
+            reader.readLine();
+            for(int i=0 ; i < size ; i++){
+                String[] friends =reader.readLine().split("-");
+                if( customerUsername.equals(friends[0])){
                     return true;
                 }
             }
@@ -63,14 +81,109 @@ public class SendRequestFile {
     }
 
     public void sendDiscountCodeToRequestSender(String friendUsername){
-
+        int size = getFileSize();
         file= new File("sendRequestToFriend.txt");
         try (BufferedReader reader =  new BufferedReader(new FileReader(file))){
             reader.readLine();
-            String[] line = reader.readLine().split("-");
-            if(line[1].equals(friendUsername)){
-                SendEmailController sendEmailController = new SendEmailController("Your discount code is: "+line[2],
-                        line[0]);
+            for (int i=0 ; i < size ; i++){
+                String[] line = reader.readLine().split("-");
+                if(line[1].equals(friendUsername)){
+                    SendEmailController sendEmailController = new SendEmailController("Your discount code is: "+line[2],
+                            line[0]);
+                }
+            }
+
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public boolean codeIsTrueForFriend(String code){
+        int size = getFileSize();
+        file= new File("sendRequestToFriend.txt");
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))){
+            reader.readLine();
+            for(int i=0 ; i < size ; i++){
+                String[] line = reader.readLine().split("-");
+                if(line[3].equals(code)){
+                    return true;
+                }
+            }
+
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+        return false;
+    }
+
+    public boolean codeIsTrueForCustomer(String code){
+        int size = getFileSize();
+        file= new File("sendRequestToFriend.txt");
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))){
+            reader.readLine();
+            for(int i=0 ; i < size ; i++){
+                String[] line = reader.readLine().split("-");
+                if(line[2].equals(code)){
+                    return true;
+                }
+            }
+
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+        return false;
+    }
+
+    public void deleteCustomerUsername(Customer customer){
+        int size = getFileSize();
+        ArrayList<String> requests= new ArrayList<>();
+        file= new File("sendRequestToFriend.txt");
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))){
+            reader.readLine();
+            for (int i=0 ; i < size ; i++) {
+                String[] line = reader.readLine().split("-");
+                if (line[0].equals(customer.getUsername())) {
+                    requests.add("*-" + line[1] + "-" + line[2] + "-" + line[3]);
+                } else
+                    requests.add(line[0] + "-" + line[1] + "-" + line[2] + "-" + line[3]);
+            }
+
+
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+
+        writeInFileAfterEditing(requests);
+    }
+    public void deleteFriendUsername(Customer friend){
+        int size = getFileSize();
+        ArrayList<String> requests= new ArrayList<>();
+        file= new File("sendRequestToFriend.txt");
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))){
+            reader.readLine();
+            for (int i=0 ; i < size ; i++){
+                String[] line = reader.readLine().split("-");
+                if(line[1].equals(friend.getUsername())){
+                    requests.add(line[0]+"-*-"+line[2]+"-"+line[3]);
+                }
+                else
+                    requests.add(line[0]+"-"+line[1]+"-"+line[2]+"-"+line[3]);
+            }
+
+
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+
+        writeInFileAfterEditing(requests);
+    }
+
+    public void writeInFileAfterEditing(ArrayList<String> requests){
+        file = new File("sendRequestToFriend.txt");
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))){
+            for (int i=0 ; i < requests.size() ; i++){
+                writer.newLine();
+                writer.write(requests.get(i));
             }
 
         }catch (IOException e){
